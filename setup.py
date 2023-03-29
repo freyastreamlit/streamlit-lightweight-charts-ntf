088182718ec0e0b86eb18386c6615660b7e62134
Joe Rosa <joe.rosa@itpmngt.co.uk>
2023-03-29 20:11:57 +0100

    Recursion

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ def package_files(directory):
 
 extra_files = package_files('streamlit_lightweight_charts_ntf/frontend/build')
 
+print('extra_files',extra_files)
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
-
-print('find_packages',setuptools.find_packages())
 
 setuptools.setup(
     name="streamlit-lightweight-charts-ntf",
@@ -27,7 +27,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/freyastreamlit/streamlit-lightweight-charts-ntf",
     packages=['streamlit_lightweight_charts_ntf'],
-    package_data={'streamlit_lightweight_charts_ntf': ['frontend/build/*.*'] },
+    package_dir={'streamlit_lightweight_charts_ntf': 'streamlit_lightweight_charts_ntf'},
+    package_data={
+        'streamlit_lightweight_charts_ntf' : ['frontend/build/*']
+        # '': ['*'],
+        # 'streamlit_lightweight_charts_ntf.frontend.build.static.js': ['*']
+    },
     include_package_data=True,
     python_requires=">=3.6",
     install_requires=[

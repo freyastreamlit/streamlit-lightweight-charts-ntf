8755df625043c1732fbed78117e93a2b1508ff32
Joe Rosa <joe.rosa@itpmngt.co.uk>
2023-03-29 23:29:06 +0100

    Clean setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
 import os
 import setuptools
 
-def package_files(directory):
-    paths = []
-    for (path, directories, filenames) in os.walk(directory):
-        for filename in filenames:
-            paths.append(os.path.join('..', path, filename))
-    return paths
-
-extra_files = package_files('streamlit_lightweight_charts_ntf/frontend/build')
-
-print('extra_files',extra_files)
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+# There is sdist and bdist
+# sdist is with source - package_data is used
+# bdist is with whl file, MANIFEST.in is used
 
 setuptools.setup(
     name="streamlit-lightweight-charts-ntf",
@@ -28,26 +21,9 @@ setuptools.setup(
     url="https://github.com/freyastreamlit/streamlit-lightweight-charts-ntf",
     include_package_data = True,
     packages=['streamlit_lightweight_charts_ntf'],
-    # packages=['streamlit_lightweight_charts_ntf','streamlit_lightweight_charts_ntf.frontend','streamlit_lightweight_charts_ntf.frontend.build'],
-    # package_dir={'streamlit_lightweight_charts_ntf': 'streamlit_lightweight_charts_ntf'},
     package_data={
         'streamlit_lightweight_charts_ntf': ['frontend/build/*','frontend/build/static/js/*'],
-        # 'streamlit_lightweight_charts_ntf.frontend': ['**/'],
-        # 'streamlit_lightweight_charts_ntf.frontend.build': [
-        #     "**/*.html",
-        #     # 'frontend/build/static/js/*',
-        #     # 'frontend/build/index.html',
-        # ],
     },
-
-    # package_data={
-    #     'streamlit_lightweight_charts_ntf' : ['frontend/build/*'],
-    #     # 'streamlit_lightweight_charts_ntf' : ['frontend/yarn.lock','frontend/package.json','frontend/build/index.html'],
-    #     # 'streamlit_lightweight_charts_ntf.frontend' : ['streamlit_lightweight_charts_ntf/frontend/build/index.html'],
-
-    #     # '': ['*'],
-    #     # 'streamlit_lightweight_charts_ntf.frontend.build.static.js': ['*']
-    # },
     python_requires=">=3.6",
     install_requires=[
         "streamlit >= 0.62",

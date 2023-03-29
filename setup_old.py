52fcd9c0fa341b7e3e33be4ae74e56d3ba307927
Joe Rosa <joe.rosa@itpmngt.co.uk>
2023-03-29 22:16:06 +0100

    Attempt with pyproject.toml

--- setup_old.py
+++ setup_old.py
@@ -29,13 +29,21 @@ setuptools.setup(
     packages=['streamlit_lightweight_charts_ntf'],
     # package_dir={'streamlit_lightweight_charts_ntf': 'streamlit_lightweight_charts_ntf'},
     package_data={
-        'streamlit_lightweight_charts_ntf' : ['frontend/build/*'],
-        # 'streamlit_lightweight_charts_ntf' : ['frontend/yarn.lock','frontend/package.json','frontend/build/index.html'],
-        # 'streamlit_lightweight_charts_ntf.frontend' : ['streamlit_lightweight_charts_ntf/frontend/build/index.html'],
-
-        # '': ['*'],
-        # 'streamlit_lightweight_charts_ntf.frontend.build.static.js': ['*']
+        'streamlit_lightweight_charts_ntf': [
+            'frontend/build/*.*',
+            'frontend/build/static/js/*.*',
+            'frontend/build/index.html',
+        ],
     },
+
+    # package_data={
+    #     'streamlit_lightweight_charts_ntf' : ['frontend/build/*'],
+    #     # 'streamlit_lightweight_charts_ntf' : ['frontend/yarn.lock','frontend/package.json','frontend/build/index.html'],
+    #     # 'streamlit_lightweight_charts_ntf.frontend' : ['streamlit_lightweight_charts_ntf/frontend/build/index.html'],
+
+    #     # '': ['*'],
+    #     # 'streamlit_lightweight_charts_ntf.frontend.build.static.js': ['*']
+    # },
     include_package_data=True,
     python_requires=">=3.6",
     install_requires=[
